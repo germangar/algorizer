771c61ad309e0ff9a355cbed5790418a7e233308
germangar <germangar@gmail.com>
2025-06-02 12:46:12 +0200

    base for sending webhook alerts

--- broker_alerts.py
+++ broker_alerts.py
@@ -1,5 +1,13 @@
 import requests
 
-
-
 url = 'https://webhook.site/8cf90478-6c35-4eb4-ac61-e72417ac5a3b'
+alert = 'This text'
+
+headers = {
+    'Content-Type': 'text/plain; charset=utf-8'
+}
+
+req = requests.post(url, data=alert, headers=headers)
+
+# Optionally, you can print the response from the server
+print(req.status_code, req.text)
