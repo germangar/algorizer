83c11f7b98b3f123abc57185c3c92ef03d9b4aaa
germangar <germangar@gmail.com>
2025-06-30 01:13:50 +0200

    Revert "allow accesing generated series values as if they were python arrays."
    This reverts commit cb74414bd5188c79b1d3fb95ab006c1e7f01d416.

--- framework/nameseries.py
+++ framework/nameseries.py
@@ -3,21 +3,10 @@ from . import active
 
 class series_c(np.ndarray):
     def __new__( cls, input_array, name:str, assignable:bool=True, index=-1 ):
-
-        assert name is not None, f"The whole point of named series_c is to have a name"
-
-        if input_array is None:
-            if name in active.timeframe.registeredSeries.keys():
-                raise ValueError( f"Can not create a new column. Series [{name}] already exists.")
-            assignable = True
-            index = active.timeframe.createColumn()
-            input_array = active.timeframe.dataset[:, index]
-
-        if input_array.shape[0] != active.timeframe.dataset.shape[0]:
-            raise ValueError( f"series_c. Input array [{name}] lenght doesn't match the dataset lenght")
-
         obj = np.array(input_array, copy=False).view(cls)
 
+        # Asserts
+        assert name is not None, f"The whole point of named series_c is to have a name"
         assert obj.base is not None, f"The NamedArray {name} is not a view of the original array"
 
         # Try to find the index of the array in the dataset columns
@@ -36,11 +25,9 @@ class series_c(np.ndarray):
             index = active.timeframe.createColumn()
 
         # Add the name property to the instance
-        obj.timeframe = active.timeframe
         obj.name = name
         obj.index = index
         obj.assignable = assignable
-        
 
         # check if it's already registered in the timeframe, otherwise register it
         if name not in active.timeframe.registeredSeries.keys():
@@ -53,24 +40,7 @@ class series_c(np.ndarray):
         self.name = getattr(obj, 'name', None)
         self.index = getattr(obj, 'index', -1)
         self.assignable = True # getattr(obj, 'assignable', False)
-        self.timeframe = getattr(obj, 'timeframe', None)
 
-    def __getitem__(self, key):
-        tf = getattr(self, 'timeframe', None)
-        atf = active.timeframe
-        if tf == atf:
-            return super().__getitem__(key)
-        
-        # the series belongs to a different timeframe
-        if isinstance( key, (float, int) ):
-            if key < 0:
-                key = active.timeframe.barindex + 1 + key
-            timestamp = active.timeframe.timestamp - ((active.timeframe.barindex - key) * active.timeframe.timeframeMsec)
-            key = self.timeframe.indexForTimestamp(timestamp)
-            return super().__getitem__(key)
-
-        raise ValueError( "Only single value access is allowed from a different timeframe.")
-        
     def __setitem__(self, key, value):
         if not self.assignable:
             raise ValueError( f"Assigning values to series_c [{self.name}] is not allowed.")
@@ -84,3 +54,9 @@ class series_c(np.ndarray):
             if np.shares_memory(dataset[:, idx], candidate_col) or np.all(dataset[:, idx] == candidate_col):
                 return idx
         return None
+
+
+
+
+# Create a NamedArray from a column of the 2D array
+# named_column = NamedArray(data_2d[:, 0], name="first_column")
